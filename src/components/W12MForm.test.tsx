631cff1d0f478ceb8111ac69ebc0b970ec3d77c4
smithaagarwal <smitha.kotian@gmail.com>
2023-12-17 13:07:56 +0000

    add test to check if onSubmit is called when the button is clicked and form is submitted

--- src/components/W12MForm.test.tsx
+++ src/components/W12MForm.test.tsx
@@ -1,5 +1,6 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import W12MForm from "./W12MForm";
+import userEvent from "@testing-library/user-event";
 
 test("renders form element", () => {
   // we can hold onto the object returned from render()
@@ -9,4 +10,21 @@ test("renders form element", () => {
   // the container is just a normal DOM element, so we can look at normal properties like '.firstChild'
   // for example, the firstChild of our container should be our form element
   expect(container.firstChild).toHaveClass("w12MForm");
+});
+
+describe("Form Submit", () => {
+  it("test clicking the button triggers the onSubmit handler of the form", async () => {
+    const onSubmitMock = jest.fn();
+    const { container } = render(<W12MForm />);
+    container.onsubmit = onSubmitMock;
+    const submitButton = screen
+      .getAllByRole("button")
+      .find((button) => button.textContent === "Submit");
+    expect(submitButton).toBeInTheDocument();
+
+    if (submitButton) {
+      fireEvent.click(submitButton);
+    }
+    expect(onSubmitMock).toBeCalled;
+  });
 });
